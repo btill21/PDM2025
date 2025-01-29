811aa4a4d55ea54797d53efdde2f03d1b3275792
Brice Tillman <157168944+btill21@users.noreply.github.com>
2025-01-28 21:53:14 -0600

    Add files via upload

---
+++ 2463/PDM2025/2463/sketch2.js
@@ -0,0 +1,27 @@
+function setup() {
+  
+    //size of our canvas x,y
+    createCanvas(720,400);
+    
+    // bg color 255 being white
+    background(255);
+    
+    // no stroke to remove outline
+    noStroke();
+    
+    //r,b,g, hue/transp
+    fill(255, 0, 0, 67);  
+    
+    //circle x,y,z
+    circle(200, 111, 150);
+    
+    fill(0, 255, 0, 67);  
+    
+    //circle x,y,z
+    circle(250, 200, 150);
+    
+    fill(0, 0, 255, 67);  
+    //circle x,y,z
+    circle(150, 200, 150);
+  }
+  
